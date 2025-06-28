e3e178c5d27c735fa30db745c7f4ca7cbd350e82
princejha05 <163818937+princejha05@users.noreply.github.com>
2025-06-28 21:11:54 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -244,7 +244,7 @@ Nᴀᴍᴇ - {}"""
 
 𝗠𝗲𝘀𝘀𝗮𝗴𝗲 <b>: {}</b>"""
 
-    CAPTION = """<b>📂 ғɪʟᴇɴᴀᴍᴇ : {file_name}
+    CAPTION = """<b>@princemoviebot 📂 ғɪʟᴇɴᴀᴍᴇ : {file_name}
 
 <b>⚙️ sɪᴢᴇ : {file_size}</b>
 <b>😶‍🌫 Mods Channel: @Dashing_Mods</b>""" 
